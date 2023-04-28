3f08713c4b66a68d43de92aaff0f7b5de4aedca7
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2023-04-28 18:59:04 +0600

    Started lesson_8. Done upto 15:50.

---
+++ lesson_8/src/app/api/apiSlice.js
@@ -0,0 +1,48 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { setCredentials, logOut } from '../../features/auth/authSlice' // importing 'setCredentials' and 'logout' reducer
+
+// works like axios request with options
+const baseQuery = fetchBaseQuery({
+   baseUrl: 'http://localhost:3500',
+   credentials: 'include', //this is to send all the cookies that are saved to browser along with every request
+   prepareHeaders: (headers, { getState }) => { // destructured getState
+      const token = getState().auth.token // get token from state if it exists. If used with authSlice, will have token, else it will not do anything
+      if(token){
+         headers.set("authorization", `Bearer ${token}`) // set token as authorization - Bearer token in header. Works only if token exists
+      }
+      return headers
+   }
+})
+
+// this is a wrapper for the base query, which will get a new "accessToken" if the old one has expired and "refreshToken" is still valid
+// works the same way as axios interceptors in the previous chapters
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+   let result = await baseQuery(args, api, extraOptions)
+
+   if(result?.error?.originalStatus === 403){ // if the initial query received a status code of 403
+      console.log("Sending Refresh Token")
+      // send refresh token to get new access token
+      const refreshResult = await baseQuery('/refresh', api, extraOptions)
+      console.log(refreshResult)
+      if(refreshResult?.data){
+         const user = api.getState().auth.user // just get the user name/data from state and store it in a variable. Has nothing to do with the API call
+         // store the new token along with the user name/data
+         api.dispatch(setCredentials({...refreshResult.data, user}))
+         // retry the original query with new access token
+         result = await baseQuery(args, api, extraOptions)
+      }
+   }
+   else{
+      //so if token has expired but the API call to "/refresh" also fails(not 403 status code), then run/dispatch logOut()
+      api.dispatch(logOut())
+   }
+
+   return result // return result of original API call if token was successfully refreshed
+}
+
+// we will create the apiSlice(for RTK. "createSlice" on the other hand can only be used with thunks and extra reducers)
+// but we won't define any endpoints here since we will be using extended API slice
+export const apiSlice = createApi({
+   baseQuery: baseQueryWithReauth,
+   endpoints: builder => ({})
+})
---
+++ lesson_8/src/features/auth/authSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// slice containing the name of the slice, the initial state, and the reducers we will be using to modify the state
+const authSlice = createSlice({
+   name: 'auth',
+   initialState: { user: null, token: null },
+   reducers: {
+      // defining the reducers(methods that modify the slice state) we will be using to change states
+      setCredentials: (state, action) => {
+         const { user, accessToken } = action.payload
+         state.user = user
+         state.token = token
+      },
+      logOut: (state, action) => {
+         state.user = null
+         state.token = null
+      }
+   }
+})
+
+// exporting the reducers
+export const { setCredentials, logOut } = authSlice.actions
+
+// exporting the slice
+export default authSlice.reducer
+
+// Custom selectors
+export const selectCurrentUser = (state) => state.auth.user
+export const selectCurrentToken = (state) => state.auth.token
